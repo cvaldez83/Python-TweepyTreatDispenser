53e4b6f9bdead53c8a2231f0a4dd01d6924b71da
Cesar <cvaldez83@gmail.com>
2018-11-21 16:16:02 -0800

    added media path to conv2mp4.py

--- conv2mp4.py
+++ conv2mp4.py
@@ -5,8 +5,8 @@ import shlex
 def run():
 
     # completed_video= os.path.join(save_path, filename)
-    filename = os.path.join('media', 'charliedetected')
-    # filename = 'media/charliedetected'
+    filename= 'media/charliedetected'
+    
 
     #Conversion to usable file format
     print("Converting to mp4")
